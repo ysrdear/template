40cac3011a18d05746614d926db5c2139894a25e
ysrdear <ysrdear@163.com>
2023-08-30 14:15:10 +0800

    fix: axios更新

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 import { BASE_URL } from "@/utils/base";
 import { ElMessage } from "element-plus";
-import { validateRes, getResData,isFile } from "@/utils/util";
+import { validateRes, getResData, isFile } from "@/utils/util";
 import { ElLoading } from "element-plus";
 import router from "@/router";
-
 
 axios.defaults.baseURL = BASE_URL;
 
@@ -39,36 +38,41 @@ axiosInstance.interceptors.request.use((config) => {
 
 //响应拦截
 axiosInstance.interceptors.response.use(
-    (response) => {
-      if (response.data && validateRes(response)) {
-        loading.close();
-        return isFile(response) ? response : getResData(response.data);
-      } else {
-        loading.close();
-        ElMessage({
-          message: response.data.msg,
-          type: "warning",
-        });
-
-        if (response.data.code === "1401") {
-          router.push("/error");
-        }
-        return Promise.reject(response.data.msg);
-      }
-    },
-    (err) => {
+  (response) => {
+    if (response.data && validateRes(response)) {
+      loading.close();
+      return isFile(response) ? response : getResData(response.data);
+    } else {
       loading.close();
       ElMessage({
-        message: err.message,
+        message: response.data.msg,
         type: "warning",
       });
+
+      if (response.data.code === "1401") {
+        router.push("/error");
+      }
+      return Promise.reject(response.data.msg);
     }
+  },
+  (err) => {
+    loading.close();
+    ElMessage({
+      message: err.message,
+      type: "warning",
+    });
+  }
 );
 
 export default function ({ url, method, ...config }, data) {
+  let sendKey = {
+    post: "data",
+    get: "params",
+  };
+
   return axiosInstance({
     method: method || "post",
-    data: data,
+    [sendKey[method]]: data,
     url: url,
     ...config,
   });
